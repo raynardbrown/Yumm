cab63022adb28624cbc8dc165ac66e2ca3756b26
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:59:47 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/yumm/view/RecipeCardIngredientActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.yumm.view;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.yumm.R;
+import com.example.android.yumm.model.Recipe;
+
+public class RecipeCardIngredientActivity extends AppCompatActivity
+{
+  private Recipe recipe;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+
+    Intent intent = getIntent();
+
+    recipe = intent.getParcelableExtra(getString(R.string.recipe_type_key));
+
+    setContentView(R.layout.activity_recipe_card_ingredient);
+  }
+
+  @Override
+  public void onAttachFragment(Fragment fragment)
+  {
+    super.onAttachFragment(fragment);
+
+    RecipeCardIngredientFragment recipeCardIngredientFragment = (RecipeCardIngredientFragment)fragment;
+
+    recipeCardIngredientFragment.setRecipeIngredientList(recipe.getRecipeIngredients());
+  }
+}
