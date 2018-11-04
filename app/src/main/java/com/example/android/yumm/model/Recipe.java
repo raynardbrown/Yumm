05d1ca12eca71f7cf7a968e574f18c2c675cc4c7
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:40:15 -0600

    Implemented parcelable and equals.

--- app/src/main/java/com/example/android/yumm/model/Recipe.java
+++ app/src/main/java/com/example/android/yumm/model/Recipe.java
@@ -1,8 +1,11 @@
 package com.example.android.yumm.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
-public class Recipe
+public class Recipe implements Parcelable
 {
   private int id;
 
@@ -30,6 +33,48 @@ public class Recipe
     this.servings = servings;
     this.imagePath = imagePath;
   }
+
+  private Recipe(Parcel in)
+  {
+    id = in.readInt();
+    name = in.readString();
+    recipeIngredientList = in.createTypedArrayList(RecipeIngredient.CREATOR);
+    recipeStepList = in.createTypedArrayList(RecipeStep.CREATOR);
+    servings = in.readInt();
+    imagePath = in.readString();
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags)
+  {
+    dest.writeInt(id);
+    dest.writeString(name);
+    dest.writeTypedList(recipeIngredientList);
+    dest.writeTypedList(recipeStepList);
+    dest.writeInt(servings);
+    dest.writeString(imagePath);
+  }
+
+  @Override
+  public int describeContents()
+  {
+    return 0;
+  }
+
+  public static final Creator<Recipe> CREATOR = new Creator<Recipe>()
+  {
+    @Override
+    public Recipe createFromParcel(Parcel in)
+    {
+      return new Recipe(in);
+    }
+
+    @Override
+    public Recipe[] newArray(int size)
+    {
+      return new Recipe[size];
+    }
+  };
 
   public int getId()
   {
@@ -89,5 +134,61 @@ public class Recipe
   public void setImagePath(String imagePath)
   {
     this.imagePath = imagePath;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if(obj == this)
+    {
+      return true;
+    }
+
+    if(obj instanceof Recipe)
+    {
+      Recipe r = (Recipe)obj;
+
+      if(this.recipeIngredientList.size() != r.recipeIngredientList.size())
+      {
+        return false;
+      }
+
+      if(this.recipeStepList.size() != r.recipeStepList.size())
+      {
+        return false;
+      }
+
+      for(int i = 0; i < this.recipeIngredientList.size(); ++i)
+      {
+        RecipeIngredient thisRecipeIngredient = this.recipeIngredientList.get(i);
+        RecipeIngredient otherRecipeIngredient = r.recipeIngredientList.get(i);
+
+        if(!thisRecipeIngredient.equals(otherRecipeIngredient))
+        {
+          return false;
+        }
+      }
+
+      for(int i = 0; i < this.recipeStepList.size(); ++i)
+      {
+        RecipeStep thisRecipeStep = this.recipeStepList.get(i);
+        RecipeStep otherRecipeStep = r.recipeStepList.get(i);
+
+        if(!thisRecipeStep.equals(otherRecipeStep))
+        {
+          return false;
+        }
+      }
+
+      return this.id == r.id &&
+              this.name.equals(r.name) &&
+              this.servings == r.servings &&
+              this.imagePath.equals(r.imagePath);
+    }
+    else
+    {
+      // not a recipe object
+      return false;
+    }
   }
 }
