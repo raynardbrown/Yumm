95088505f5f51ff2998120c43bda7b1b60790a96
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:47:55 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/yumm/utils/YummConstants.java
@@ -0,0 +1,11 @@
+package com.example.android.yumm.utils;
+
+public class YummConstants
+{
+  public static int PHONE_SIZED_DEVICE = 1;
+  public static int TABLET_SIZED_DEVICE = 2;
+
+  private YummConstants()
+  {
+  }
+}
