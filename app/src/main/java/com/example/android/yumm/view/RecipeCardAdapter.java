6002d1ea2638dfde30780c482ffef26e0445a72f
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:53:09 -0600

    Added a click listener for the recipe list items. Added an imageview to a list item if an image resource is available.

--- app/src/main/java/com/example/android/yumm/view/RecipeCardAdapter.java
+++ app/src/main/java/com/example/android/yumm/view/RecipeCardAdapter.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.android.yumm.R;
 import com.example.android.yumm.databinding.RecipeCardListItemBinding;
 import com.example.android.yumm.model.Recipe;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 import java.util.Locale;
@@ -16,10 +19,13 @@ import java.util.Locale;
 public class RecipeCardAdapter extends RecyclerView.Adapter<RecipeCardAdapter.RecipeCardViewHolder>
 {
   private List<Recipe> recipeList;
+  private IRecipeCardListItemClickListener recipeCardListItemClickListener;
 
-  RecipeCardAdapter(List<Recipe> recipeList)
+  RecipeCardAdapter(List<Recipe> recipeList,
+                    IRecipeCardListItemClickListener recipeCardListItemClickListener)
   {
     this.recipeList = recipeList;
+    this.recipeCardListItemClickListener = recipeCardListItemClickListener;
   }
 
   @Override
@@ -39,6 +45,24 @@ public class RecipeCardAdapter extends RecyclerView.Adapter<RecipeCardAdapter.Re
   @Override
   public void onBindViewHolder(RecipeCardViewHolder holder, int position)
   {
+    // Add the image if it is available
+
+    final String imagePath = recipeList.get(position).getImagePath();
+    if(imagePath != null && imagePath.length() > 0)
+    {
+      ImageView imageView = holder.itemBinding.ivRecipeCardItemImage;
+
+      imageView.setVisibility(View.VISIBLE);
+
+      Picasso.with(imageView.getContext())
+              .load(imagePath)
+              .into(imageView);
+    }
+    else
+    {
+      holder.itemBinding.ivRecipeCardItemImage.setVisibility(View.GONE);
+    }
+
     holder.itemBinding.tvRecipeCardItemTitle.setText(recipeList.get(position).getName());
     holder.itemBinding.tvRecipeCardItemServings.setText(String.format(Locale.getDefault(), "%d", recipeList.get(position).getServings()));
   }
@@ -49,14 +73,26 @@ public class RecipeCardAdapter extends RecyclerView.Adapter<RecipeCardAdapter.Re
     return recipeList.size();
   }
 
-  class RecipeCardViewHolder extends RecyclerView.ViewHolder
+  class RecipeCardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
   {
     RecipeCardListItemBinding itemBinding;
 
     RecipeCardViewHolder(RecipeCardListItemBinding itemBinding)
     {
       super(itemBinding.getRoot());
+
       this.itemBinding = itemBinding;
+
+      this.itemBinding.getRoot().setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+      final int clickedIndex = getAdapterPosition();
+      Recipe recipe = recipeList.get(clickedIndex);
+
+      RecipeCardAdapter.this.recipeCardListItemClickListener.onRecipeCardListItemClick(recipe);
     }
   }
 }
