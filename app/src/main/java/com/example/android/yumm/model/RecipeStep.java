8f8b9aea3a4da26f0ddfc8ef876680014063a5e5
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:43:09 -0600

    Implemented parcelable and equals.

--- app/src/main/java/com/example/android/yumm/model/RecipeStep.java
+++ app/src/main/java/com/example/android/yumm/model/RecipeStep.java
@@ -1,6 +1,9 @@
 package com.example.android.yumm.model;
 
-public class RecipeStep
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class RecipeStep implements Parcelable
 {
   private int id;
 
@@ -24,6 +27,46 @@ public class RecipeStep
     this.videoUrl = videoUrl;
     this.thumbnailUrl = thumbnailUrl;
   }
+
+  protected RecipeStep(Parcel in)
+  {
+    id = in.readInt();
+    shortDescription = in.readString();
+    description = in.readString();
+    videoUrl = in.readString();
+    thumbnailUrl = in.readString();
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags)
+  {
+    dest.writeInt(id);
+    dest.writeString(shortDescription);
+    dest.writeString(description);
+    dest.writeString(videoUrl);
+    dest.writeString(thumbnailUrl);
+  }
+
+  @Override
+  public int describeContents()
+  {
+    return 0;
+  }
+
+  public static final Creator<RecipeStep> CREATOR = new Creator<RecipeStep>()
+  {
+    @Override
+    public RecipeStep createFromParcel(Parcel in)
+    {
+      return new RecipeStep(in);
+    }
+
+    @Override
+    public RecipeStep[] newArray(int size)
+    {
+      return new RecipeStep[size];
+    }
+  };
 
   public int getId()
   {
@@ -73,5 +116,29 @@ public class RecipeStep
   public void setThumbnailUrl(String thumbnailUrl)
   {
     this.thumbnailUrl = thumbnailUrl;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if(obj == this)
+    {
+      return true;
+    }
+
+    if(obj instanceof RecipeStep)
+    {
+      RecipeStep otherRecipeStep = (RecipeStep)obj;
+
+      return this.id == otherRecipeStep.id &&
+              this.shortDescription.equals(otherRecipeStep.shortDescription) &&
+              this.description.equals(otherRecipeStep.description) &&
+              this.videoUrl.equals(otherRecipeStep.videoUrl) &&
+              this.thumbnailUrl.equals(otherRecipeStep.thumbnailUrl);
+    }
+    else
+    {
+      return false;
+    }
   }
 }
