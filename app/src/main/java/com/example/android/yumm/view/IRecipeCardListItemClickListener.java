10fb25b62c04af251d56a4a4fa5c497d2233528d
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:49:46 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/yumm/view/IRecipeCardListItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.android.yumm.view;
+
+import com.example.android.yumm.model.Recipe;
+
+public interface IRecipeCardListItemClickListener
+{
+  public void onRecipeCardListItemClick(Recipe recipe);
+}
