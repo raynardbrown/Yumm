e17aa7f7568d7b08d8847de67b885098dce8b9cb
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2018-11-04 04:44:09 -0600

    Initial commit.

---
+++ app/src/main/java/com/example/android/yumm/receivers/INetworkBroadcastReceiverListener.java
@@ -0,0 +1,8 @@
+package com.example.android.yumm.receivers;
+
+public interface INetworkBroadcastReceiverListener
+{
+  public void onNetworkConnected();
+
+  public void onNetworkDisconnected();
+}
